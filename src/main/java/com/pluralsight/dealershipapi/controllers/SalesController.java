8b0c658ff7d09568dabbccf8da2f2c5b7b9900ab
Yiming0576 <g1097420948@gmail.com>
2024-12-11 13:12:25 -0500

    add some annotations for my controllers

--- src/main/java/com/pluralsight/dealershipapi/controllers/SalesController.java
+++ src/main/java/com/pluralsight/dealershipapi/controllers/SalesController.java
@@ -1,4 +1,10 @@
 package com.pluralsight.dealershipapi.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sales")
 public class SalesController {
 }
